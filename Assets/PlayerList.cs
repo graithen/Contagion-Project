0990a0d1e1d6942caf6c44d842ae15c71bf6bb79
Graithen <grey.jim@gmail.com>
2019-11-16 12:21:42 +0000

    Initial project push
    Pushing all project files.

---
+++ Assets/PlayerList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PlayerList : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "";
+
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            text.text += player.NickName + "\n";
+        }
+    }
+}
