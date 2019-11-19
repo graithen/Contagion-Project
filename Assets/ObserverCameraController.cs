0e6c7d231432221687ba1fff8b3a5fd16371ba5b
Graithen <grey.jim@gmail.com>
2019-11-19 16:07:28 +0000

    Created observer camera
    Created a controller that can be used by observers to view the scene.

---
+++ Assets/ObserverCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObserverCameraController : MonoBehaviour
+{
+    public float CameraSpeed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        transform.position = new Vector2(transform.position.x + (CameraSpeed * Input.GetAxis("Horizontal") * Time.deltaTime), transform.position.y + (CameraSpeed * Input.GetAxis("Vertical") * Time.deltaTime));
+    }
+}
