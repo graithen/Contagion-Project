cf771cd3fbb93620b2db0fb1cad0826339576e1c
James <grey.jim@gmail.com>
2020-01-24 19:24:06 +0000

    Can now spawn as observer or player
    Can now spawn in as a player or observer from lobby.

---
+++ Assets/ControllerSelection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerSelection : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Observer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("PlayerType") == 0)
+            Player.transform.parent = null;
+        if (PlayerPrefs.GetInt("PlayerType") == 1)
+            Observer.transform.parent = null;
+
+        Destroy(this.gameObject);
+    }
+}
